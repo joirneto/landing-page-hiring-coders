3699a3d4c691ea5b002dbfab252f8a0634f0908a
joirneto <joirneto@gmail.com>
2021-07-16 10:29:25 -0300

    Refatorando

---
+++ js/script.js
@@ -0,0 +1,55 @@
+const form = document.getElementById('form-js');
+
+
+form.addEventListener('submit', (e)=> {
+  e.preventDefault();
+
+  let name = document.getElementById('name').value;
+  let email = document.getElementById('email').value;
+
+  if(validator(name,email))addItem(name, email);
+
+});
+
+function validator(name,email){
+  if((name !== null && name !== '') && (email !== null && email !== '')){
+    return true;
+  }
+  else{
+   alert("Prencha os campos solicitados!") ;
+  }
+}
+
+function atualizaPage(){
+  let divForm = document.getElementById('div-form-js');
+
+  let loader = `<img src="public/images/Loader.gif" alt="loader" width="320px" height="280px">`
+  let pronto = `<p id = "pronto" > PRONTO </p>`
+ 
+  divForm.innerHTML = loader
+
+  setTimeout(() => {
+    divForm.innerHTML = pronto
+  },1000);
+}
+
+function addItem(name,email) {
+  let data = {
+    name,
+    email
+  };
+
+  if (localStorage.getItem('lead') === null) {
+    localStorage.setItem('lead', JSON.stringify([data]));
+  } else {
+    localStorage.setItem(
+      'lead',
+      JSON.stringify([
+        ...JSON.parse(localStorage.getItem('lead')),
+            data
+        ])
+      );
+    }
+    atualizaPage();
+}
+  
